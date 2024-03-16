afe5bbc8546e5e17216b3974ed3d62eec07d0602
SUBHIN-TM <subhintm2018@gmail.com>
2024-03-16 13:45:50 +0530

    jwt setting

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,21 +1,27 @@
 import User from '../models/userSchema.js';
-import bcrypt from 'bcrypt' 
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken"
+
 export const loginPost = async (req, res) => {
   try {
     console.log('To Do Login Section');
     console.log(req.body);
-    const mail=req.body.name
-    const password=req.body.password
-    const existing=await User.findOne({mail:mail})
-    console.log(existing);
-    if(!existing){
-    return res.json({invalidUser:true})
+    const mail = req.body.name
+    const password = req.body.password
+    const existing = await User.findOne({ mail: mail })
+   
+    if (!existing) {
+      return res.json({ invalidUser: true })
     }
     const passwordMatch = await bcrypt.compare(password, existing.password)
-    if(!passwordMatch){
-      return res.json({passwordMissmatch:true})
-    }else{
-      return res.json({dashboard:true});
+    if (!passwordMatch) {
+      return res.json({ passwordMissmatch: true })
+    } else {
+      console.log(existing);
+       const payload={id:existing._id,name:existing.name,mail:existing.mail}
+     let token=  jwt.sign(payload,"todosecret",{expiresIn:'24h'})
+       console.log(token);
+      return res.json({ dashboard: true ,token});
     }
   } catch (error) {
     console.error('Error from login post page');
@@ -27,33 +33,58 @@ export const signupPost = async (req, res) => {
     console.log('To Do signup Section');
     console.log(req.body);
     let { name, password, mail } = req.body;
-     const existing=await User.findOne({mail:mail})
-     if(existing){
-        console.log("mail exist");
-       return res.json({mailExists:true})
-     }else{
-         password = await bcrypt.hash(password, 10)
-        const user = new User({
-            name,
-            password,
-            mail,
-          });
-          const result = await user.save();
-          console.log("Registered",result);
-          return res.json({registered:true,result});
-     }
-   
+    const existing = await User.findOne({ mail: mail })
+    if (existing) {
+      console.log("mail exist");
+      return res.json({ mailExists: true })
+    } else {
+      password = await bcrypt.hash(password, 10)
+      const user = new User({
+        name,
+        password,
+        mail,
+      });
+      const result = await user.save();
+      console.log("Registered", result);
+      return res.json({ registered: true, result });
+    }
+
   } catch (error) {
-    console.error('Error from signup post page',error);
+    console.error('Error from signup post page', error);
   }
 };
 
 
-export const dashboardGet =async(req,res)=>{
+export const dashboardGet = async (req, res) => {
   try {
-      console.log("Dashboard");
-      return res.json({success:true})
+    console.log("Dashboard");
+    return res.json({ success: true })
   } catch (error) {
-    console.error('Error from dashboardGet page',error);
+    console.error('Error from dashboardGet page', error);
   }
 }
+
+export const addTask = async (req, res) => {
+  try {
+    console.log("Task adding section");
+    const { date, task, status } = req.body.tasks
+    console.log(date, task, status);
+
+  } catch (error) {
+    console.error('Error from addTask page', error);
+  }
+
+}
+
+
+export const logout = async (req, res) => {
+  try {
+    console.log("Logout  section");
+  
+
+  } catch (error) {
+    console.error('Error from logout page', error);
+  }
+
+}
+
--- routers/users.js
+++ routers/users.js
@@ -1,11 +1,14 @@
 import express from 'express' ;
-import { loginPost, signupPost,dashboardGet } from '../controllers/userControllers.js';
+import { loginPost, signupPost,dashboardGet,addTask ,logout} from '../controllers/userControllers.js';
 
 const router = express.Router();
 
 router.post('/login', loginPost);
 router.post('/signup', signupPost);
+router.post('/addTask', addTask);
 router.get('/dashboard', dashboardGet);
+router.get('/logout', logout);
+
 
 
 
